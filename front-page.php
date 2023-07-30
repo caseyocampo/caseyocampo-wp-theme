6975286713bdbefb1071ae84bb2eda700aeebcac
Casey Ocampo <caseysoflo@gmail.com>
2023-07-29 22:24:39 -0400

    fix recent posts fotn size

--- front-page.php
+++ front-page.php
@@ -14,7 +14,7 @@ get_header();
         <h1 style="margin-top: 1em; padding-bottom: 1em; border-bottom: 1px solid var(--border-color)">I’m an <span class="primary-color">Accessibility Engineer</span> who writes about tech and personal finance.</h1>
 
         <div class="page--content">
-            <h2 style="margin-bottom: 2em;">Recent Posts <img src="https://www.caseyocampo.com/wp-content/uploads/2023/01/icon-blog.svg" alt="" class="icon-blog"></h2>
+            <h2 class="h1" style="margin-bottom: 1em;">Recent Posts <img src="https://www.caseyocampo.com/wp-content/uploads/2023/01/icon-blog.svg" alt="" class="icon-blog"></h2>
 
             <?php
             if (have_posts()) {
