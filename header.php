82f70f6a17965cf39ab325d7e2c9108615b6830e
Casey Ocampo <caseysoflo@gmail.com>
2024-03-10 23:43:31 -0400

    chore: fix variable color updates

--- header.php
+++ header.php
@@ -53,7 +53,9 @@
 
             <div class="right-nav">
                 <button id="nav-btn" aria-label="primary menu" aria-expanded="false">
-                    <p>Menu</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+                        <path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path>
+                    </svg>
                 </button>
 
                 <?php
