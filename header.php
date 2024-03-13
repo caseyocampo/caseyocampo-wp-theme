2d95dd52ec67b5e5f882e89d09ab8226030689da
Casey Ocampo <caseysoflo@gmail.com>
2024-03-13 19:04:02 -0400

    fix: tailwind config to include main dir path and remove google analytics

--- header.php
+++ header.php
@@ -5,29 +5,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="theme-color" content="#ffffff">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-
-    <meta name="google-site-verification" content="yTzqIHzTSkyUeSxm_fpADrUVk9EacY5p-nVKfdlRTp4" />
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-217821750-1"> </script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-217821750-1');
-    </script>
-
     <meta property="og:type" content="article">
     <meta name="description" content="Casey Ocampo's blog.">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <?php
     wp_head();
     ?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}", "./*.php", "./template-parts/*.php"],
+  content: ["./src/**/*.{html,js,php}", "./*.php", "./template-parts/*.php", "./*.{html,js,php}"],
   theme: {
     extend: {},
   },
