7222fdd9ec24f5a2ed88fb1579fd3238e5440cb1
Casey Ocampo <caseysoflo@gmail.com>
2024-03-15 13:27:15 -0400

    feat: add google analytics

--- header.php
+++ header.php
@@ -10,6 +10,18 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0V4CSP11R7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-0V4CSP11R7');
+    </script>
     <?php
     wp_head();
     ?>
